a2494dc238dec02ae0b12dc0adde8d7533d1e00e
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2021-09-29 17:41:57 -0300

    <edition> The SlateEditor's DefaultElement component font size has been increased

--- src/components/SlateEditor/components/textComponents/DefaultElement.tsx
+++ src/components/SlateEditor/components/textComponents/DefaultElement.tsx
@@ -1,5 +1,8 @@
 const DefaultElement = props => {
-  return <p {...props.attributes} style={{ textAlign: "justify" }}>{props.children}</p>
+  return <p {...props.attributes} style={{ 
+    textAlign: "justify",
+    fontSize: '1.3rem'
+  }}>{props.children}</p>
 }
 
 export default DefaultElement;
